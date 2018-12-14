9186f58b65006f33ac51b30b39a4f9c09c588d00
Eduard Karpa <31408020+edmlbox@users.noreply.github.com>
2018-12-14 22:45:01 +0200

    Update lockScrren.js

--- script/lockScrren.js
+++ script/lockScrren.js
@@ -2,25 +2,30 @@
 
 var getAll = chrome.storage.sync;
 
-getAll.get(null,(x1,x2)=>{
-    console.log(x1,x2)
+getAll.get(null, (x1, x2) => {
+  console.log(x1, x2);
 
-    if(x1.lockStatus === true){
-       
-        var unLockWalPPP = document.getElementById('unLockWalPPP');
-        unLockWalPPP.onkeyup = ()=>{
-            console.log(unLockWalPPP.value)
+  if (x1.lockStatus === true) {
+    var unLockWalPPP = document.getElementById("unLockWalPPP");
+    unLockWalPPP.onkeyup = () => {
+      console.log(unLockWalPPP.value);
 
-            if(unLockWalPPP.value === x1.lock){ 
-                window.close('lock.html')
-                window.open("popup.html", "_blank", "left=800,top=200,width=320,height=430,status=no,scrollbars=no,menubar=no,resizable=no,titlebar=no,toolbar=no");
-            }
-            
-        }
-
-    }else{
-        console.log('x1.lockStatus === false')
-        window.close('lock.html')
-        window.open("popup.html", "_blank", "left=800,top=200,width=320,height=430,status=no,scrollbars=no,menubar=no,resizable=no,titlebar=no,toolbar=no");
-    }
-})
+      if (unLockWalPPP.value === x1.lock) {
+        window.close("lock.html");
+        window.open(
+          "popup.html",
+          "_blank",
+          "left=800,top=200,width=320,height=430,status=no,scrollbars=no,menubar=no,resizable=no,titlebar=no,toolbar=no"
+        );
+      }
+    };
+  } else {
+    console.log("x1.lockStatus === false");
+    window.close("lock.html");
+    window.open(
+      "popup.html",
+      "_blank",
+      "left=800,top=200,width=320,height=430,status=no,scrollbars=no,menubar=no,resizable=no,titlebar=no,toolbar=no"
+    );
+  }
+});
